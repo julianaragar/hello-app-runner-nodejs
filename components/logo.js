b4bb711ef31710c8d831362137831962ef32788e
Julian <julianaragr@gmail.com>
2023-06-06 18:17:37 +0200

    new version

--- components/logo.js
+++ components/logo.js
@@ -7,7 +7,7 @@ export default function Logo({ size }) {
     <Image
       priority
       //src="/images/banner_base_light.png"
-      src="Screenshot-2023-06-06-at-17-15-15.png"
+      src="/images/Screenshot-2023-06-06-at-17-15-15.png"
       width={size.w}
       height={size.h}
       alt="Powered by App Runner"
